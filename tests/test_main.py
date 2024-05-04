f028d8dc30ea17a377c31fcf8d9fd6d95e1b7116
nhatnm0612 <nhatnguyenminh061289@gmail.com>
2024-05-04 11:04:19 +0700

    [UPD] python tests

---
+++ tests/test_main.py
@@ -0,0 +1,38 @@
+"""Testing code"""
+
+from fastapi.testclient import TestClient
+from dev.main import app
+
+
+client = TestClient(app=app)
+
+
+def test_get_hello():
+    """Testing code"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"hello": "Fake LM Studio API"}
+
+
+def test_post_completions():
+    """Testing code"""
+    response = client.post(
+        "/v1/chat/completions",
+        headers={"content-type": "application/json"},
+        json={
+            "temperature": 0.1,
+            "max_tokens": 12,
+            "stream": False,
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "what is the last letter of the alphabet?",
+                }
+            ]
+        }
+    )
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "id" in response_json.keys()
+    assert "created" in response_json.keys()
+    assert "choices" in response_json.keys()
